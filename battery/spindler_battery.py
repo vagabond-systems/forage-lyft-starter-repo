a8f09c21ecb76134c751844a3a622e40d09448da
bjrshussain@outlook.com <bjrshussain@outlook.com>
2022-07-01 20:43:55 -0500

    previosuly added 3 years service criteria by mistake

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -7,7 +7,7 @@ class SpindlerBattery(Battery):
     def __init__(self, last_service_date: datetime) -> None:
         self.__last_service_date = last_service_date
 
-    # returns true if has been ovr 3 years since last service
+    # returns true if has been ovr 2 years since last service
     def needs_service(self) -> bool:
         service_threshold_date = self.__last_service_date.replace(year=self.__last_service_date.year + 2)
         return service_threshold_date < datetime.today().date()
