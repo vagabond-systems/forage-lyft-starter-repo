3c96f5e14dc421f03bc1634673c9e9b30a5dc8de
Alexandru Boncut <boncut.alex10@gmail.com>
2022-09-23 14:18:30 -0500

    Remove unnecessary properties

--- battery.py
+++ battery.py
@@ -1,87 +1,32 @@
+from abc import ABC, abstractmethod
 from datetime import datetime
-from serviceable import Serviceable
+from utils import add_years_to_date
 
-class Battery(Serviceable):
-    @property
-    def current_date(self) -> str:
-        pass
-
-    @current_date.setter
-    def current_date(self, date) -> None:
-        pass
-
-    @property
-    def last_service_date(self) -> str:
-        pass
-
-    @last_service_date.setter
-    def last_service_date(self, date) -> None:
+class Battery(ABC):
+    @abstractmethod
+    def needs_service(self) -> bool:
         pass
 
 #########################################################################
 
 class NubinBattery(Battery):
     def __init__(self, current_date, last_service_date):
-        self._current_date = datetime.strptime(current_date, '%Y-%m-%d').date()
-        self._last_service_date = datetime.strptime(last_service_date, '%Y-%m-%d').date()
-
-    @property
-    def current_date(self) -> str:
-        return self._current_date
-
-    @current_date.setter
-    def current_date(self, date) -> None:
-        self._current_date = date
-
-    @property
-    def last_service_date(self) -> str:
-        return self._last_service_date
-
-    @last_service_date.setter
-    def last_service_date(self, date) -> None:
-        self._last_service_date = date
-
-    ## Calculated by adding 4 years to the last_service_date
-    def next_service_date(self):
-        return datetime(
-            self.last_service_date.year + 4, 
-            self.last_service_date.month, 
-            self.last_service_date.day).date()
+        self.current_date = datetime.strptime(current_date, '%Y-%m-%d').date()
+        self.last_service_date = datetime.strptime(last_service_date, '%Y-%m-%d').date()
 
     ## Should be serviced once every 4 years
-    def needs_service(self):
-        return self._current_date >= self.next_service_date()
+    def needs_service(self) -> bool:
+        next_service_date = add_years_to_date(self.last_service_date, 4)
+        return self.current_date >= next_service_date
 
 #########################################################################
 
 class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
-        self._current_date = datetime.strptime(current_date, '%Y-%m-%d').date()
-        self._last_service_date = datetime.strptime(last_service_date, '%Y-%m-%d').date()
-
-    @property
-    def current_date(self) -> str:
-        return self._current_date
-
-    @current_date.setter
-    def current_date(self, date) -> None:
-        self._current_date = date
-
-    @property
-    def last_service_date(self) -> str:
-        return self._last_service_date
-
-    @last_service_date.setter
-    def last_service_date(self, date) -> None:
-        self._last_service_date = date
-
-    ## Calculated by adding 2 years to the last_service_date
-    def next_service_date(self):
-        return datetime(
-            self.last_service_date.year + 2, 
-            self.last_service_date.month, 
-            self.last_service_date.day).date()
+        self.current_date = datetime.strptime(current_date, '%Y-%m-%d').date()
+        self.last_service_date = datetime.strptime(last_service_date, '%Y-%m-%d').date()
 
     ## Should be serviced once every 2 years
-    def needs_service(self):
-        return self._current_date >= self.next_service_date()
+    def needs_service(self) -> bool:
+        next_service_date = add_years_to_date(this.last_service_date, 2)
+        return self.current_date >= next_service_date
--- engine.py
+++ engine.py
@@ -1,84 +1,38 @@
-from abc import abstractmethod
-from serviceable import Serviceable
+from abc import ABC, abstractmethod
 
-class Engine(Serviceable):
-    @property
-    def current_mileage(self) -> int:
-        pass
-
-    @property
-    def last_service_mileage(self) -> int:
+class Engine(ABC):
+    @abstractmethod
+    def needs_service(self) -> bool:
         pass
 
 #########################################################################
 
 class CapuletEngine(Engine):
     def __init__(self, current_mileage, last_service_mileage):
-        self._current_mileage = current_mileage
-        self._last_service_mileage = last_service_mileage
-
-    @property
-    def current_mileage(self) -> int:
-        return self._current_mileage
-
-    @current_mileage.setter
-    def current_mileage(self, value):
-        self._current_mileage = value
-
-    @property
-    def last_service_mileage(self) -> int:
-        return self._last_service_mileage
-
-    @last_service_mileage.setter
-    def last_service_mileage(self, value) -> None:
-        self._last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
 
     ## Should be serviced once every 30000 miles
     def needs_service(self) -> bool:
-        return self._current_mileage - self._last_service_mileage >= 30000
+        return self.current_mileage - self.last_service_mileage >= 30000
 
 #########################################################################
 
 class SternmanEngine(Engine):
-    def __init__(self, warning_light_is_on = False):
-        self._warning_light_is_on = warning_light_is_on
-
-    @property
-    def warning_light_is_on(self) -> bool:
-        return self._warning_light_is_on
-
-    @warning_light_is_on.setter
-    def warning_light_is_on(self, value):
-        if(value == "RESET"):
-            self._warning_light_is_on = False
+    def __init__(self, warning_light_is_on):
+        self.warning_light_is_on = warning_light_is_on
 
     ## Should be serviced only when the warning indicator is on
     def needs_service(self) -> bool:
-        return self._warning_light_is_on
+        return self.warning_light_is_on
 
 #########################################################################
 
 class WilloughbyEngine():
     def __init__(self, current_mileage, last_service_mileage):
-        self._current_mileage = current_mileage
-        self._last_service_mileage = last_service_mileage
-
-    @property
-    def current_mileage(self) -> int:
-        return self._current_mileage
-
-    @current_mileage.setter
-    def current_mileage(self, value):
-        self._current_mileage = value
-
-    @property
-    def last_service_mileage(self) -> int:
-        return self._last_service_mileage
-
-    @last_service_mileage.setter
-    def last_service_mileage(self, value) -> None:
-        self._last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
 
     ## Should be serviced once every 60000 miles
     def needs_service(self) -> bool:
-        return self._current_mileage - self._last_service_mileage >= 60000
+        return self.current_mileage - self.last_service_mileage >= 60000
