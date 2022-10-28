d0290495b06c54bb1d5d34d9750c76371724386b
ria-oha <74937073+ria-oha@users.noreply.github.com>
2022-10-28 18:24:31 -0400

    Update and rename serviceable/carFactory.py to carFactory.py

---
+++ carFactory.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from engine.capulet_engine import CapuletEngine
+class Calliope(CapuletEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+from datetime import datetime
+from engine.willoughby_engine import WilloughbyEngine
+class Glissade(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+    
+
+from datetime import datetime
+from engine.sternman_engine import SternmanEngine
+class Palindrome(SternmanEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+          
+from datetime import datetime
+from engine.willoughby_engine import WilloughbyEngine
+class Rorschach(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+          
+          
+from datetime import datetime
+from engine.capulet_engine import CapuletEngine
+class Thovex(CapuletEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
