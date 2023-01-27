7b5f83e2f4416dd1ebcd4ae227b1b6d292c413a5
Casey Spears <92146309+powerwild@users.noreply.github.com>
2023-01-27 16:05:55 -0600

    Update capulet_engine.py

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,15 @@
-from abc import ABC
-
-from car import Car
+import os
 
 
-class CapuletEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+
+class CapuletEngine():
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 30000
+    def needs_service(self, val):
+        self.current_mileage = val
+        return self.current_mileage - self.last_service_mileage >= os.environ.get('CAPULET')
+
+    def perform_service(self):
+        self.last_service_mileage = self.current_mileage
