9131a299b7e9e9545e30889ba67cd2427fcf34d0
Casey Spears <92146309+powerwild@users.noreply.github.com>
2023-01-27 16:17:47 -0600

    Update sternman_engine.py

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,4 +1,6 @@
-class SternmanEngine():
+from engine.engine import Engine
+
+class SternmanEngine(Engine):
     def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
