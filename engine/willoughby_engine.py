8469e655b5c51620f1d1ff63c10885ccf20350a7
Casey Spears <92146309+powerwild@users.noreply.github.com>
2023-01-27 16:18:09 -0600

    Update willoughby_engine.py

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,7 +1,8 @@
 import os
+from engine.engine import Engine
 
 
-class WilloughbyEngine():
+class WilloughbyEngine(Engine):
     def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
