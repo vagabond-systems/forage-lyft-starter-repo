48195c2415161eab7049aed56844fd31f928e656
Emanuele Regnani <emanueleregnani1234@gmail.com>
2023-01-28 17:57:24 +0100

    tests

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,7 +1,9 @@
 from abc import ABC
 
-from car import Car
+#import sys, os
+#sys.path.append(os.path.relpath("../../"))
 
+from car import Car
 
 class CapuletEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
@@ -11,3 +13,5 @@ class CapuletEngine(Car, ABC):
 
     def engine_should_be_serviced(self):
         return self.current_mileage - self.last_service_mileage > 30000
+
+capEn = CapuletEngine()
