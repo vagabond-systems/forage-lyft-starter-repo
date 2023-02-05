c3ac43ba95e383a6d308ed7c25231209bfc2d25c
MohammedNagdy <mohammednagdy@hotmail.com>
2023-02-05 17:47:23 +0200

    Added static decorator for car factory methods

--- car_factory.py
+++ car_factory.py
@@ -6,26 +6,31 @@ from .car import Car
 class CarFactory:
 
   # Creating different types of cars
+  @staticmethod
   def create_calliope(self, current_date, last_service_date, current_mileage,
                       last_service_mileage):
     return Car(splinder.Splinder(last_service_date, current_date),
                capulet.Capulet(last_service_mileage, current_mileage))
 
+  @staticmethod
   def create_glissade(self, current_date, last_service_date, current_mileage,
                       last_service_mileage):
     return Car(splinder.Splinder(last_service_date, current_date),
                willoughby.Willoughby(last_service_mileage, current_mileage))
 
+  @staticmethod
   def create_palindrome(self, current_date, last_service_date,
                         warning_lights_on):
     return Car(splinder.Splinder(last_service_date, current_date),
                sternman.Sternman(warning_lights_on))
 
+  @staticmethod
   def create_rorschach(self, current_date, last_service_date, current_mileage,
                        last_service_mileage):
     return Car(nubbin.Nubbin(last_service_date, current_date),
                willoughby.Willoughby(last_service_mileage, current_mileage))
 
+  @staticmethod
   def create_thovex(self, current_date, last_service_date, current_mileage,
                     last_service_mileage):
     return Car(nubbin.Nubbin(last_service_date, current_date),
