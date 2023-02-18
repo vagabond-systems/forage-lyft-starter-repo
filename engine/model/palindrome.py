58708e6fcab3648d2b35167f139e7a30b0d5bfb4
YamaSai44 <46951690+YamaSai44@users.noreply.github.com>
2023-02-18 20:28:25 +0000

    Update palindrome.py

--- engine/model/palindrome.py
+++ engine/model/palindrome.py
@@ -5,7 +5,7 @@ from engine.sternman_engine import SternmanEngine
 
 class Palindrome(SternmanEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
         if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
             return True
         else:
