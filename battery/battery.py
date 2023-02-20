8b8fe3f550557020bfed859895534e9afa6c73ed
samueljazzjohn <samueljazzjohn@gmail.com>
2023-02-20 18:33:09 +0530

    initial commit

--- battery/battery.py
+++ battery/battery.py
@@ -1,7 +1,6 @@
 from abc import ABC
 
-from car import Car
 
-class Battery(Car,ABC):
-    def __init__(self,last_service_date):
-        super().__init__(last_service_date)
+class Battery(ABC):
+    def needs_service(self):
+        pass
--- engine/engine.py
+++ engine/engine.py
@@ -1,7 +1,6 @@
 from abc import ABC
 
-from car import Car
 
-class Engine(Car,ABC):
-    def __init__(self,last_service_date):
-        super().__init__(last_service_date)
+class Engine(ABC):
+    def needs_service(self):
+        pass
