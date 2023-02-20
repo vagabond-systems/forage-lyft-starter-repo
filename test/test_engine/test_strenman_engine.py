18d87e54b88de6fc185048a98d231ac0ca020ce3
samueljazzjohn <samueljazzjohn@gmail.com>
2023-02-20 23:20:32 +0530

    unit test completed

--- test/test_engine/test_strenman_engine.py
+++ test/test_engine/test_strenman_engine.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import date
 
 from engine.sternman_engine import SternmanEngine
 
@@ -6,7 +7,7 @@ from engine.sternman_engine import SternmanEngine
 class TestSternmanEngine(unittest.TestCase):
     def test_needs_service_true(self):
         warning_light_is_on = True
-        engine = SternmanEngine(warning_light_is_on)
+        engine = SternmanEngine( warning_light_is_on)
         self.assertTrue(engine.needs_service())
 
     def test_needs_service_false(self):
--- test/test_engine/test_willoughby_engine.py
+++ test/test_engine/test_willoughby_engine.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import date
 
 from engine.willoughby_engine import WilloughbyEngine
 
@@ -7,11 +8,11 @@ class TestWilloughbyEngine(unittest.TestCase):
     def test_needs_service_true(self):
         current_mileage = 60001
         last_service_mileage = 0
-        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        engine = WilloughbyEngine( current_mileage, last_service_mileage)
         self.assertTrue(engine.needs_service())
 
     def test_needs_service_false(self):
         current_mileage = 60000
         last_service_mileage = 0
-        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        engine = WilloughbyEngine( current_mileage, last_service_mileage)
         self.assertFalse(engine.needs_service())
