b82991636629f095aebe92f3a5be3724b190087f
mhamza2742 <125182294+mhamza2742@users.noreply.github.com>
2023-04-05 15:16:19 +0530

    Refactor

---
+++ Battery.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class Battery(ABC):
+
+    @abstractmethod
+    def needs_service(self):
+        pass
---
+++ Serviceable.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class Serviceable(ABC):
+
+    @abstractmethod
+    def needs_service(self):
+        pass
