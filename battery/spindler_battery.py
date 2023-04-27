e298f81436a42ab5702a1f6333e28baa0ec48c58
shreyanzjain <shreyans32@outlook.com>
2023-04-27 12:13:13 +0530

    updated the battery needs_Service methods to work based on if the current date is greater than the service_threshold_date

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -7,9 +7,11 @@ class SpindlerBattery(IBatteryStrategy):
         self.last_service_date = last_service_date
         self.current_date = current_date
         self.service_threshold_age = 2
+        self.service_threshold_date = self.last_service_date.replace(
+            year=self.last_service_date.year + self.service_threshold_age)
 
     def needs_service(self):
-        if self.current_date.year - self.last_service_date.year > self.service_threshold_age:
+        if self.current_date > self.service_threshold_date:
             return True
         else:
             return False
