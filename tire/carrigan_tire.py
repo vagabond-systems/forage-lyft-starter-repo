b9a2f7c6197266169baaf415e55662c7d92305c5
shreyanzjain <shreyans32@outlook.com>
2023-04-30 14:52:50 +0530

    Implementations of both tire types into concrete strategies

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -2,8 +2,15 @@ from tire.tireStrategy import TireStrategy
 
 
 class CarriganTire(TireStrategy):
-    def __init__(self, tire_values) -> None:
+    def __init__(self, tire_values: list) -> None:
         self.tire_values = tire_values
 
     def needs_service(self):
-        pass
+        ctr: int = 0
+        for i in self.tire_values:
+            if i >= 0.9:
+                ctr += 1
+        if ctr >= 1:
+            return True
+        else:
+            return False
--- tire/octoprime_tire.py
+++ tire/octoprime_tire.py
@@ -2,8 +2,11 @@ from tire.tireStrategy import TireStrategy
 
 
 class OctoprimeTire(TireStrategy):
-    def __init__(self, tire_values) -> None:
+    def __init__(self, tire_values: list) -> None:
         self.tire_values = tire_values
 
-    def needs_service():
-        pass
+    def needs_service(self):
+        if sum(self.tire_values) >= 3:
+            return True
+        else:
+            return False
