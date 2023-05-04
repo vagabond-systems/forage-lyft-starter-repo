4820dcd72a3b929185bb066d4afa7e3b8c64d205
Prosper <prosperorjiogo1999@gmail.com>
2023-05-04 17:07:07 +0100

    .1

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -9,5 +9,6 @@ class CapuletEngine(Car, ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 30000
+    def needs_service(self):
+        needs_service = self.current_mileage - self.last_service_date > 30000
+        return needs_service
