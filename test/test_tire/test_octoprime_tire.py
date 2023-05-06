1f946662772046b50a20b478e7894b3eea76651b
Prosper <prosperorjiogo1999@gmail.com>
2023-05-06 06:30:56 +0100

    test.1

--- test/test_tire/test_octoprime_tire.py
+++ test/test_tire/test_octoprime_tire.py
@@ -4,7 +4,13 @@ from tire.octoprime_tires import OctoprimeTire
 
 class TestOctoprimeTire(unittest.TestCase):
     def test_needs_to_be_serviced(self):
-        tire_sensor_readings = [0.2, 0.5, 0.7, 0.1]
+        tire_sensor_readings = [0.9, 0.9, 0.7, 0.1]
         
         service = OctoprimeTire(tire_sensor_readings)
         self.assertTrue(service.needs_service)
+        
+    def test_does_not_need_service(self):
+        tire_sensor_readings = [0.3, 0.5, 0.7, 0.1]
+        
+        service = OctoprimeTire(tire_sensor_readings)
+        self.assertFalse(service.needs_service)
