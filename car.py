e8c65e45ebbc5af9f9ac87b6b95ee6eeab76fcb0
Prosper Orjiogo <52451398+Tempahh@users.noreply.github.com>
2023-05-06 09:32:34 +0100

    Update car.py

--- car.py
+++ car.py
@@ -2,9 +2,10 @@ from servicable import Serviceable
 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tire):
         self.engine = engine
         self.battery = battery
+        self.tire = tire
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service()
