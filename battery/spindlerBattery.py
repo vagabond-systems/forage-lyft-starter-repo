7da81c23ff3f18a6dd9466bd652c0f32811f8176
janice1457 <chen.siyuan4@northeastern.edu>
2023-05-08 14:53:00 -0700

    Add new functionality tires

--- battery/spindlerBattery.py
+++ battery/spindlerBattery.py
@@ -8,7 +8,7 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 3)
         if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
