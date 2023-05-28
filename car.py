691b7c00a6e9732a34cc0b511c7c4c0f6516a853
Sumit Kumar <90103435+SumitK786@users.noreply.github.com>
2023-05-28 21:49:05 +0530

    commit

--- car.py
+++ car.py
@@ -1,10 +1,14 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
+class Car(Serviceable):
+  def __init__(self, engine, battery, tires):
+    self.engine = engine
+    self.battery = battery
+    self.tires = tires
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
-    @abstractmethod
-    def needs_service(self):
-        pass
+  def needs_service(self):
+    return any(
+      self.engine.needs_service(),
+      self.battery.needs_service(),
+      self.tires.needs_service()
+    ) 
