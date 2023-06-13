bb1cbca667e92954da5ec952c8ca8c6fb09ac4dd
Shevilll <ahmadfaraz00710@gmail.com>
2023-06-13 11:23:57 +0530

    reduced code repetation, formatted

--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -1,12 +1,7 @@
-from datetime import datetime
-
 from engine.willoughby_engine import WilloughbyEngine
+from utils import needs_service
 
 
 class Rorschach(WilloughbyEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        return needs_service(self)
