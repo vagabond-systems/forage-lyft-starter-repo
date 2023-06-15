b395fa788489dfb6661541599030feedbd48bcf8
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-06-16 02:53:59 +0530

    Update test_car.py

--- test_car.py
+++ test_car.py
@@ -8,6 +8,26 @@ class TestCar(unittest.TestCase):
         engine = Engine("V8")
         battery = Battery("Lithium-ion")
         self.car = Car("Model XYZ", engine, battery)
+    
+    def test_carriganTireServiceWhenWorn(self):
+        engine = Engine("V8")
+        battery = Battery("Lithium-ion")
+        car = Car("Model XYZ", engine, battery)
+
+        tireWear = [0.8, 0.7, 0.95, 0.6]  # Example tire wear array for Carrigan tires
+        car.setTireWear(tireWear, "Carrigan")
+
+        self.assertTrue(car.needsTireService())
+   
+    def test_octoprimeTireServiceWhenWorn(self):
+        engine = Engine("V8")
+        battery = Battery("Lithium-ion")
+        car = Car("Model XYZ", engine, battery)
+
+        tireWear = [0.5, 0.8, 0.6, 1.2]  # Example tire wear array for Octoprime tires
+        car.setTireWear(tireWear, "Octoprime")
+
+        self.assertTrue(car.needsTireService())
 
     def test_getModel(self):
         self.assertEqual(self.car.getModel(), "Model XYZ")
