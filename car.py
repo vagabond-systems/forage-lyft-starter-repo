3b5d0239c4cca0ac703eeb1d52c918575089e683
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-06-16 02:52:56 +0530

    Update car.py

--- car.py
+++ car.py
@@ -12,4 +12,19 @@ class Car:
 
     def getBattery(self):
         return self.battery
+# car.py
+
+class CarFactory:
+    # Existing code...
+
+    def needsTireService(self, tireWear: List[float], tireType: str) -> bool:
+        if tireType == "Carrigan":
+            return any(wear >= 0.9 for wear in tireWear)
+        elif tireType == "Octoprime":
+            return sum(tireWear) >= 3
+        else:
+            raise ValueError("Invalid tire type")
+
+    
+
 
