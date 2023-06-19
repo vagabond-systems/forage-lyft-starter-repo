4fba3e2eae7956df1ffbe042232a53327c4c29fa
shreyanzjain <shreyans32@outlook.com>
2023-06-19 10:54:50 +0530

    update "[instead of looping over all tires and after that returning true or false, we could just loop over the tires, if we find a tire value >0.9 we return true, else false]"

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -9,8 +9,5 @@ class CarriganTire(ITireStrategy):
         ctr: int = 0
         for i in self.tire_values:
             if i >= 0.9:
-                ctr += 1
-        if ctr >= 1:
-            return True
-        else:
-            return False
+                return True
+        return False
