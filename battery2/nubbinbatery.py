e89541db4e2b69484f19b84f5e80b602bf40b8db
srcode03 <saraiker_b21@ce.vjti.ac.in>
2023-06-28 16:43:00 +0530

    Code Refactor

---
+++ battery2/nubbinbatery.py
@@ -0,0 +1,15 @@
+from utils import add_years_to_date
+from battery2.battery import Battery
+
+
+class NubbinBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_services(self):
+        date_which_battery_serviced_by = add_years_to_date(self.last_service_date + 2)
+        if date_which_battery_serviced_by < self.current_date:
+            return True
+        else:
+            return False
---
+++ engine2/capuletengine.py
@@ -0,0 +1,10 @@
+from engine2.engine import Engine
+
+
+class CapuletEngine(Engine):
+    def __init__(self, current_milage, last_service_milage):
+        self.last_service_milage = last_service_milage
+        self.current_milage = current_milage
+
+    def need_services(self):
+        return self.current_milage - self.last_service_milage > 30000
