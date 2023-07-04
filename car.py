4877f3bd600fc8d701528c2535f7053f278d363b
bepratham <prathamsinghkathayat@email.com>
2023-07-04 17:44:27 +0530

    task 4 done

--- car.py
+++ car.py
@@ -2,9 +2,10 @@ from serviceable import Serviceable
 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
