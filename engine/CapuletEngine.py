bf4f7e0007b4f4af1c5cf9b4e15156f16750d5af
Rume7 <electengrhume@gmail.com>
2023-07-15 17:29:57 +0100

    Improved needs_service method to raise errors.

--- engine/CapuletEngine.py
+++ engine/CapuletEngine.py
@@ -1,12 +1,17 @@
 from engine.IEngine import IEngine
 
+
 class CapuletEngine(IEngine):
-    
-    def __init__(self, last_service_mileage, current_mileage):
+
+    def __init__(self, last_service_date, last_service_mileage, current_mileage):
+        super().__init__(last_service_date)
         self.last_service_mileage = last_service_mileage
         self.current_mileage = current_mileage
-        
-        
+
     def needs_service(self):
-        return self.current_mileage - self.last_service_mileage >= 30000
+        mileage_difference = self.current_mileage - self.last_service_mileage
+        if mileage_difference < 0:
+            raise ValueError('Mileage difference should not be negative')
+        else:
+            return mileage_difference >= 30000
 
--- engine/IEngine.py
+++ engine/IEngine.py
@@ -1,12 +1,11 @@
 from abc import ABCMeta, abstractmethod
 
+
 class IEngine(metaclass=ABCMeta):
-    
+
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
-        
-    
+
     @abstractmethod
     def needs_service(self):
         pass
-    
