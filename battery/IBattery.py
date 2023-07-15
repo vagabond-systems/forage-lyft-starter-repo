77f5a61b87b6ca573ae266e79c84a0cbe4453204
Rume7 <electengrhume@gmail.com>
2023-07-15 16:33:48 +0100

    Corrected need_service method in NubbinBattery class.

--- battery/IBattery.py
+++ battery/IBattery.py
@@ -1,14 +1,18 @@
 from abc import ABCMeta, abstractmethod
 
+
 class IBattery(metaclass=ABCMeta):
-    
+
     def __init__(self, last_service_date, current_date) -> None:
         self.last_service_date = last_service_date
         self.current_date = current_date
-        
-    
+
+    def get_current_date(self):
+        return self.current_date
+
+    def get_last_service_date(self):
+        return self.last_service_date
+
     @abstractmethod
     def needs_service():
         pass
-    
-    
--- battery/NubbinBattery.py
+++ battery/NubbinBattery.py
@@ -1,11 +1,11 @@
 from battery.IBattery import IBattery
 
+
 class NubbinBattery(IBattery):
-    
+
     def __init__(self, last_service_date, current_date):
         super().__init__(last_service_date, current_date)
-        
-        
-    def needs_service(self):
-        return (self.current_date - self.last_service_date) % 2 == 0
 
+    def needs_service(self):
+        difference_in_years = self.get_current_date().year - self.get_last_service_date().year
+        return difference_in_years % 4 == 0
