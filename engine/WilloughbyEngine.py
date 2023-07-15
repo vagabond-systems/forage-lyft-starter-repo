64fb4a6569e0c720c5324ed07c9548e1ca88dc92
Rume7 <electengrhume@gmail.com>
2023-07-15 09:19:08 +0100

    Added the Engine Interface and implemented inherited classes.

---
+++ engine/WilloughbyEngine.py
@@ -0,0 +1,12 @@
+from engine.IEngine import IEngine
+
+class WilloughbyEngine(IEngine):
+    
+    def __init__(self, last_service_mileage, current_mileage):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+        
+        
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage >= 60000
+
