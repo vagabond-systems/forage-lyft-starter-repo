1a2e609322d255e5c81f7fabcb672527541db294
bilele123123 <tple06203@gmail.com>
2023-07-16 23:37:19 -0700

    Change service requirement of Spindler battery from 3 to 2.

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -6,7 +6,7 @@ from battery.spindler_battery import SpindlerBattery
 class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service_true(self):
         current_date = date.fromisoformat("2023-07-15")
-        last_service_date = date.fromisoformat("2021-05-15")
+        last_service_date = date.fromisoformat("2020-05-15")
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
     
