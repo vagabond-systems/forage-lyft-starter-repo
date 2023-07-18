950f98a896a5fb9d7a5705a12b0b7b63c750e8e6
Maharshi Bhatnagar <119435144+Maharshibhatnagar@users.noreply.github.com>
2023-07-18 14:22:21 +0530

    Add files via upload

---
+++ engine/engine_type/sternman_engine.py
@@ -0,0 +1,12 @@
+from engine.engine import Engine
+
+
+class SternmanEngine(Engine):
+    def __init__(self, warning_light_is_on):
+        self.warning_light_is_on = warning_light_is_on
+
+    def needs_service(self):
+        if self.warning_light_is_on:
+            return True
+        else:
+            return False
