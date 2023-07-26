da536f32473b1414e29c2f6ae42bff82a8f88efb
KISHAN94700 <95291243+KISHAN94700@users.noreply.github.com>
2023-07-26 17:13:28 +0530

    Update glissade.py

--- engine/model/glissade.py
+++ engine/model/glissade.py
@@ -5,7 +5,7 @@ from engine.willoughby_engine import WilloughbyEngine
 
 class Glissade(WilloughbyEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
         if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
             return True
         else:
