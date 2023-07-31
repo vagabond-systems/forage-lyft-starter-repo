49bb097c9462a69b5b2d49a133d7eabb925a4a64
inishantxchandel <nishantxchandel@gmail.com>
2023-07-31 13:31:09 +0530

    changes according to new design

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,7 +1,6 @@
 from abc import ABC
-
 from car import Car
-
+from datetime import datetime
 
 class CapuletEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
@@ -9,5 +8,6 @@ class CapuletEngine(Car, ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
+        # Service criteria for CapuletEngine: Once every 30,000 miles
         return self.current_mileage - self.last_service_mileage > 30000
