209f4b86928864c3af372f0abc9d8a151bd420b4
deepakjuit <181337@juitsolan.in>
2023-08-06 18:36:57 +0530

    resolved fix - static method

--- model/car_factory.py
+++ model/car_factory.py
@@ -7,13 +7,22 @@ from battery.spindler_battery import SpindlerBattery
 from battery.nubbin_battery import NubbinBattery
 
 class CarFactory:
+    @staticmethod
     def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car: 
         return Car(CapuletEngine(current_mileage,last_service_mileage), SpindlerBattery(current_date, last_service_date))
+    
+    @staticmethod
     def create_glissade(self,current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car: 
         return Car(WilloughbyEngine(current_mileage,last_service_mileage), SpindlerBattery(current_date, last_service_date))
+    
+    @staticmethod
     def create_palindrome(self, current_date: date, last_service_date: date, warning_light_on: bool) -> Car: 
         return Car(SternmanEngine(warning_light_on), SpindlerBattery(current_date, last_service_date))
+    
+    @staticmethod
     def create_rorschach(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car: 
         return Car(WilloughbyEngine(current_mileage,last_service_mileage), NubbinBattery(current_date, last_service_date))
+    
+    @staticmethod
     def create_thovex(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
         return Car(CapuletEngine(current_mileage,last_service_mileage), SpindlerBattery(current_date, last_service_date))
