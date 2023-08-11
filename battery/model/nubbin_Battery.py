9bc8b7485f6b0894923ea1f036023985f5acf3b9
Vijay-krishna-G <140730878+Vijay-krishna-G@users.noreply.github.com>
2023-08-11 09:48:16 +0530

    Add files via upload

---
+++ battery/model/nubbin_Battery.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from car import Car
+
+
+class NubbinBattery(Car,ABC):
+     def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+        
+     def battery_should_be_serviced(self):
+        return self.last_service_date - self.current_date > 4
+    
+        
---
+++ battery/model/palindrome.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from spinder_Battery import SpinderBattery
+
+class Palindrome(SpinderBattery):
+    def needs_service(self,):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.battery_should_be_serviced():
+            return True
+        else:
+            return False
+
+    
