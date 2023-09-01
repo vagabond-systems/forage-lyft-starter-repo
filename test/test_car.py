f28e700e3462c9f622328671c00963d9be0c1d68
29deepanshutyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2023-09-02 00:01:24 +0530

    unit test case changes

--- test/test_car.py
+++ test/test_car.py
@@ -6,6 +6,50 @@ from engine.model.glissade import Glissade
 from engine.model.palindrome import Palindrome
 from engine.model.rorschach import Rorschach
 from engine.model.thovex import Thovex
+import unittest
+from datetime import datetime
+
+from engine.model.calliope import Calliope
+from engine.model.glissade import Glissade
+from engine.model.palindrome import Palindrome
+from engine.model.rorschach import Rorschach
+from engine.model.thovex import Thovex
+
+
+class TestEngine(unittest.TestCase):
+    def test_engine_should_be_serviced_when_mileage_is_over_limit(self):
+        car = Calliope(datetime.today().date(), 30001, 0)
+        self.assertTrue(car.engine.needs_service())
+
+    def test_engine_should_not_be_serviced_when_mileage_is_under_limit(self):
+        car = Calliope(datetime.today().date(), 29999, 0)
+        self.assertFalse(car.engine.needs_service())
+
+    def test_engine_should_be_serviced_when_last_service_date_is_overdue(self):
+        car = Calliope(datetime.today().date() - datetime.timedelta(days=365), 0, 0)
+        self.assertTrue(car.engine.needs_service())
+
+    def test_engine_should_not_be_serviced_when_last_service_date_is_not_overdue(self):
+        car = Calliope(datetime.today().date() - datetime.timedelta(days=30), 0, 0)
+        self.assertFalse(car.engine.needs_service())
+
+
+class TestBattery(unittest.TestCase):
+    def test_battery_should_be_serviced_when_age_is_over_limit(self):
+        car = Calliope(datetime.today().date(), 0, 365)
+        self.assertTrue(car.battery.needs_service())
+
+    def test_battery_should_not_be_serviced_when_age_is_under_limit(self):
+        car = Calliope(datetime.today().date(), 0, 300)
+        self.assertFalse(car.battery.needs_service())
+
+    def test_battery_should_be_serviced_when_warning_light_is_on(self):
+        car = Calliope(datetime.today().date(), 0, 0, True)
+        self.assertTrue(car.battery.needs_service())
+
+    def test_battery_should_not_be_serviced_when_warning_light_is_off(self):
+        car = Calliope(datetime.today().date(), 0, 0, False)
+        self.assertFalse(car.battery.needs_service())
 
 
 class TestCalliope(unittest.TestCase):
