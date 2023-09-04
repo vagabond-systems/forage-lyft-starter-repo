c41a1988d202f466cb1f1e4dbe8d8a5618fd7afc
JackCraigUNSW <127163294+JackCraigUNSW@users.noreply.github.com>
2023-09-04 13:33:22 +1000

    Implemented batteries and engines

---
+++ engine/engine_type/willoughby_engine.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from engine import Engine
+
+
+class WilloughbyEngine(Engine, ABC):
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 60000
