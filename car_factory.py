52bbb2bf1c83ffc44114763eaefae67e20fc37ad
JackCraigUNSW <127163294+JackCraigUNSW@users.noreply.github.com>
2023-09-04 13:12:00 +1000

    scaffold car factory

---
+++ car_factory.py
@@ -0,0 +1,22 @@
+
+
+class CarFactory():
+    @staticmethod
+    def create_calliope():
+        pass
+
+    @staticmethod
+    def create_glissade():
+        pass
+    
+    @staticmethod
+    def create_palindrome():
+        pass
+
+    @staticmethod
+    def create_rorschach():
+        pass
+
+    @staticmethod
+    def create_thovex():
+        pass
