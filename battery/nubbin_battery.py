7d74dcff7ad09c54206e44dc6e637b2f1fbc0f56
Vishnu Sudheer <vishnusudheer1234@gmail.com>
2023-10-15 02:53:32 +0530

    file added

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,11 +1,15 @@
-from abc import ABC
+from battery.battery import Battery
+from utils import add_years_to_date
 
-from car import Car
 
-class NubbinBattery(Car, ABC):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
-    def battery_should_be_serviced(self):
-        return self.current_date - self.last_service_date > 4
+    def needs_service(self):
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,11 +1,15 @@
-from abc import ABC
+from battery.battery import Battery
+from utils import add_years_to_date
 
-from car import Car
 
-class SpindlerBattery(Car, ABC):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
-    def battery_should_be_serviced(self):
-        return self.current_date - self.last_service_date > 2
+    def needs_service(self):
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
