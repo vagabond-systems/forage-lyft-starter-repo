d1215075672413046a8d2b53354270625ed1652f
Vishnu Sudheer <vishnusudheer1234@gmail.com>
2023-10-15 02:30:09 +0530

    added battery

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,11 @@
+from abc import ABC
+
+from car import Car
+
+class NubbinBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def battery_should_be_serviced(self):
+        return self.current_date - self.last_service_date > 4
