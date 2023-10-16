db76fbdef1d68f1a98951ef9d9b673a53dd90d1a
Anmol Choudhary <ac038960@gmail.com>
2023-10-16 22:05:31 +0530

    updated

---
+++ test/test_tires/test_carrigan_tires.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tires.carrigan_tires import CarriganTires
+
+
+class TestCarriganTires(unittest.TestCase):
+    def test_needs_service_true(self):
+        tire_wear = [0.1, 0.3, 0.2, 0.9]
+        tires = CarriganTires(tire_wear)
+        self.assertTrue(tires.needs_service())
+
+    def test_needs_service_false(self):
+        tire_wear = [0.1, 0.2, 0.4, 0.2]
+        tires = CarriganTires(tire_wear)
+        self.assertFalse(tires.needs_service())
---
+++ test/test_tires/test_octoprime_tires.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tires.octoprime_tires import OctoprimeTires
+
+
+class TestOctoprimeTires(unittest.TestCase):
+    def test_needs_service_true(self):
+        tire_wear = [0.8, 0.8, 0.8, 0.7]
+        tires = OctoprimeTires(tire_wear)
+        self.assertTrue(tires.needs_service())
+
+    def test_needs_service_false(self):
+        tire_wear = [0.1, 0.2, 0.4, 0.2]
+        tires = OctoprimeTires(tire_wear)
+        self.assertFalse(tires.needs_service())
