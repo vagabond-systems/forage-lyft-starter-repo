94a45a0de0b72498faceb552b033e22009770793
alenadang <3alena@gmail.com>
2023-10-18 19:56:04 +0800

    create test files

---
+++ test/test_batteries.py
@@ -0,0 +1,21 @@
+# test only concrete implementations of the engine and battery classes for this task
+# may assume all inputs to the system are valid 
+# (i.e all parameters are the expected type and all values are within reasonable bounds).
+
+import unittest
+from datetime import datetime
+import batteries
+
+class TestSpindler(unittest.TestCase):
+    def test_needs_service(self):
+        pass
+
+    def test_does_not_need_service(self):
+        pass
+
+class TestNubbin(unittest.TestCase):
+    def test_needs_service(self):
+        pass
+
+    def test_does_not_need_service(self):
+        pass
---
+++ test/test_engines.py
@@ -0,0 +1,28 @@
+# test only concrete implementations of the engine and battery classes for this task
+# may assume all inputs to the system are valid 
+# (i.e all parameters are the expected type and all values are within reasonable bounds).
+
+import unittest
+from datetime import datetime
+import engines
+
+class TestCapulet(unittest.TestCase):
+    def test_needs_service(self):
+        pass
+
+    def test_does_not_need_service(self):
+        pass
+
+class TestWilloughby(unittest.TestCase):
+    def test_needs_service(self):
+        pass
+
+    def test_does_not_need_service(self):
+        pass
+
+class TestSternman(unittest.TestCase):
+    def test_needs_service(self):
+        pass
+
+    def test_does_not_need_service(self):
+        pass
