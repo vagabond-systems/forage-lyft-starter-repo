a4e467ead8e207501944de119a1a1665d8288d8e
matthew-keicher <108882364+matthew-keicher@users.noreply.github.com>
2023-11-04 21:24:08 -0400

    Task 4 -Upgraded Spindler battery -Created tire package -Create unit tests for tires

---
+++ tires/carrigan_tires.py
@@ -0,0 +1,11 @@
+from tires.tires import Tires
+
+class CarriganTires(Tires):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        for tire in self.tire_wear:
+            if tire >= 0.9:
+                return True
+        return False
---
+++ tires/octoprime_tires.py
@@ -0,0 +1,8 @@
+from tires.tires import Tires
+
+class OctoprimeTires(Tires):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        return sum(self.tire_wear) >= 3.0
