953cdcccf24fc669b0a78ec728e9d39c8875aae2
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-11-13 06:29:44 +0530

    Final Commit

--- car_module/car.py
+++ car_module/car.py
@@ -1,2 +1,11 @@
 from .serviceable_Interface import Serviceable
 from .components.battery.batter_Interface import Battery
+from .components.engine.engine_Interface import Engine
+
+class Car(Serviceable):
+    def __init__(self, engine : Engine, battery : Battery):
+        self.engine = engine
+        self.battery = battery
+    
+    def needs_service(self) -> bool:
+        return self.engine.needs_service() or self.battery.needs_service()
