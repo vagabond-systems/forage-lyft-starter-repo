c6a178dcf348e8e7c3e4656d78b0a8205896a3a8
VirajRaut <virajvraut2002@gmail.com>
2023-11-16 14:24:04 +0530

    Added car.py

---
+++ car.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Serviceable(ABC):
+    @abstractmethod
+    def needs_service(self):
+        pass
+
+
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        if self.engine.needs_service() or self.battery.needs_service():
+            return True
+        else:
+            return False
