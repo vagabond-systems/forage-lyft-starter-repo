91536a8f57c7455182be9c3ad6314a485d0c494f
VirajRaut <virajvraut2002@gmail.com>
2023-11-16 15:32:57 +0530

    Upgraded Spindler and Added Tire

--- car.py
+++ car.py
@@ -8,12 +8,13 @@ class Serviceable(ABC):
 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tire):
         self.engine = engine
         self.battery = battery
+        self.tire = tire
 
     def needs_service(self):
-        if self.engine.needs_service() or self.battery.needs_service():
+        if self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service():
             return True
         else:
             return False
