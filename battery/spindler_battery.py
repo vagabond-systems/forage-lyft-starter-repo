b78d661de215ee70a781a9e8053ee1e3c8f822d0
azukauteh <aazure263@gmail.com>
2023-11-17 08:38:17 -0800

    add new implementation/features

---
+++ battery/spindler_battery.py
@@ -0,0 +1,15 @@
+from battery.battery import Battery
+from utils import add_years_to_date
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
