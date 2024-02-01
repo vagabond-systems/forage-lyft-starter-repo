047728f2190e5e5bdc00a1e46d3b473a3c7a6cea
Frank Guarino <fguarino71@gmail.com>
2024-02-01 15:18:47 -0500

    Car

--- car.py
+++ car.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+    def __init__(self, engine, battery):
+        engine(last_service_date=engine.last_service_date, warning_light_is_on = engine.warning_light_is_on)
+        battery = battery(last_service_date=datetime.today().date())
 
     @abstractmethod
     def needs_service(self):
