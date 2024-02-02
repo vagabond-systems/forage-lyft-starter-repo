794d159ddbce1d05a380fadf43b1834f86262c05
Frank Guarino <fguarino71@gmail.com>
2024-02-02 18:24:50 -0500

    Car.py constructor fix, and needs_service method written correctly

--- car.py
+++ car.py
@@ -1,12 +1,11 @@
-from abc import ABC, abstractmethod
-from datetime import datetime
+from serviceable import Serviceable
 
 
-class Car(ABC):
+class Car(Serviceable):
     def __init__(self, engine, battery):
-        engine(last_service_date=engine.last_service_date, warning_light_is_on = engine.warning_light_is_on)
-        battery = battery(last_service_date=datetime.today().date())
+        self.engine = engine
+        self.battery = battery
 
-    @abstractmethod
+
     def needs_service(self):
-        pass
+        return self.engine.needs_service() or self.battery.needs_service()
